192f0678f69ee27595b308a76579810d25043d36
SouparnaBiswas <souparna.biswas@excellisit.net>
2025-10-07 20:49:48 +0530

    some changes

--- resources/views/frontend/partials/top_grid.blade.php
+++ resources/views/frontend/partials/top_grid.blade.php
@@ -3,15 +3,14 @@
 <div class="col-lg-3 col-md-6">
     <div class="access-div-img-wrap">
         <div class="access-div-img">
-            <img src="{{ Storage::url($top_grid->icon) }}" alt="" />
+            <img src="{{ Storage::url($top_grid->icon) }}" alt="{{$top_grid->img_alt_tag ?? ''}}" />
         </div>
         <div class="access-div-text">
             <h4>{{ $top_grid->title }}</h4>
             <p>
-                {{  $top_grid->description }} 
+                {{  $top_grid->description }}
             </p>
         </div>
     </div>
 </div>
 @endforeach
-            
